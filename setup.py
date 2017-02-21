0da0a2cfcd7213c7ee853698b13f49d469e956da
Peerchemist <dx.1178@gmail.com>
2017-02-21 17:58:28 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(name='pacli',
-      version='0.2.2',
+      version='0.2.3',
       description='Simple CLI PeerAssets client.',
-      keywords = ['peerassets', 'blockchain', 'assets', 'client'],
+      keywords=['peerassets', 'blockchain', 'assets', 'client'],
       url='https://github.com/peerassets/pacli',
       author='Peerchemist',
       author_email='peerchemist@protonmail.ch',
       license='GPL',
       packages=['pacli'],
-      install_requires=['pypeerassets', 'terminaltables'],
+      install_requires=['pypeerassets', 'terminaltables', 'appdirs'],
       entry_points={
           'console_scripts': [
               'pacli = pacli.__main__:main'
