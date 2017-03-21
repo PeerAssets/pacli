47066afd28954ba5f466ec268d1ce3aa4f243f4d
Peerchemist <dx.1178@gmail.com>
2017-03-21 11:31:18 +0100

    version bump.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pacli',
-      version='0.2.4',
+      version='0.2.5',
       description='Simple CLI PeerAssets client.',
       keywords=['peerassets', 'blockchain', 'assets', 'client'],
       url='https://github.com/peerassets/pacli',
