5ab3f4472c9af41602f99dd78cc7ee06a1f68464
Peerchemist <dx.1178@gmail.com>
2018-05-30 09:23:17 +0200

    update export_to_csv functionality

--- pacli/export.py
+++ pacli/export.py
@@ -1,4 +1,6 @@
 import csv
+from pypeerassets.pautils import exponent_to_amount
+
 
 def export_to_csv(cards, filename):
     '''export <cards> to csv <file>'''
@@ -9,11 +11,12 @@ def export_to_csv(cards, filename):
         c = card.__dict__.copy()
         c.pop("asset_specific_data")
         c["receiver"] = c["receiver"][0]
-        c["amount"] = c["amount"][0]
+        c["amount"] = exponent_to_amount(c["amount"][0], card.number_of_decimals)
+        c.pop("number_of_decimals")
         return c
 
     with open(filename, 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=';')
-        writer.writerow(format_card(cards[0]).keys())
+        writer.writerow(format_card(cards[0]).keys())  # writing the header
         for i in cards:
             writer.writerow(format_card(i).values())
