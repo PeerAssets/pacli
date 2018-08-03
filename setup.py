3dc8aa6541192453198b53603487580b89a121f5
Peerchemist <dx.1178@gmail.com>
2018-08-03 15:40:01 +0200

    update setup.py to allow for README rendering at pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 from setuptools import setup
+from os import path
+
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Financial and Insurance Industry',
+  'Programming Language :: Python',
+  'Operating System :: OS Independent',
+  'Natural Language :: English',
+  'License :: OSI Approved :: BSD License'
+]
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='pacli',
       version='0.4.4',
       description='Simple CLI PeerAssets client.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       keywords=['peerassets', 'blockchain', 'assets', 'client'],
       url='https://github.com/peerassets/pacli',
       author='Peerchemist',
