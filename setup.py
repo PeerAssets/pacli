56b00a5635c0d65afc5a5c1dcb42e0b3d8a5ce19
Peerchemist <dx.1178@gmail.com>
2018-10-06 20:41:34 +0200

    version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pacli',
-      version='0.4.5.1',
+      version='0.4.6',
       description='Simple CLI PeerAssets client.',
       long_description=long_description,
       long_description_content_type='text/markdown',
